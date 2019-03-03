c9b313519b62979b7b2ebbdfb1c7bf2d8bcf6029
mew_151 <36990947+y0sh-S@users.noreply.github.com>
2019-03-03 21:05:32 +0900

    added comments on x-value.c

--- codes/x-value.c
+++ codes/x-value.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-double x = -7.251;
-double maximum = -5.463;
-double increment = 0.001;
-double diff;
+double x = -7.251;         //Minimum endpoint for the graphs(where they intersect). Found by grapphing on desmos.com.
+double maximum = -5.463;   //Maximum endpoint for the graphs. 
+double increment = 0.001;  //Increments for the x value to increase. dx?
+double diff;               //Empty double variable for checking the difference between final x value and maximum endpoint.
 
 int main(void){
-    FILE *text;
+    FILE *text;                      //creating a file for output
     text = fopen("x-value.txt","w");
-    while(x<maximum){
-        fprintf(text,"%0.3f\n", x);
-        x+=increment;
+    while(x<maximum){                //run the function while x value is smaller than the maximum endpoint
+        fprintf(text,"%0.3f\n", x);  //print the x value into the file upto 3rd decimal place.
+        x+=increment;                //add increments to the x value, the loop continues while x value is smaller than the maximum endpoint.
     }
-    diff = x - maximum;
-    if(diff>0.001){
-        fprintf(text,"%0.3f\n", maximum);
+    diff = x - maximum;              //compare the final x value and maximum endpoint value (-5.463).
+    if(diff>0.001){                       //run the function if the difference is greater than 0.001
+        fprintf(text,"%0.3f\n", maximum); //print the maximum endpoint value into the file.
     }
     fclose(text);
     return 0;
